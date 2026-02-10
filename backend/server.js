2189c5ac163a146cb6dbf495998428793b4f05d1
nesarikartejas10 <nesarikartejas10@gmail.com>
2026-02-10 11:10:15 +0530

    setup environment variable & configuration

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import app from "./src/app.js";
+import { config } from "./src/config/envConfig.js";
 
 const startServer = () => {
-  const PORT = 3000;
+  const PORT = config.port || 5000;
   app.listen(PORT, () => {
     console.log(`Server is listening on http://localhost:${PORT}`);
   });
