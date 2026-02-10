6dc196dceb4e6f5b3c11439d3d33f5d2a2db583c
nesarikartejas10 <nesarikartejas10@gmail.com>
2026-02-10 18:46:09 +0530

    added express-mongo-sanitize as a global middleware to prevent NoSQL injection attacks

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,19 @@
 import express from "express";
 import userRouter from "./routes/user.routes.js";
 import globalErrorHandler from "./middlewares/globalErrorHandler.js";
+import mongoSanitize from "express-mongo-sanitize";
 
 const app = express();
+
+//middlewares
 app.use(express.json());
+app.use(
+  mongoSanitize({
+    replaceWith: "_",
+  }),
+);
+
+//routes
 app.use("/api/v1/auth", userRouter);
 
 //global error middleware
