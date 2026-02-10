e804437bf27ae856322d0cbf16f6e7bcdfd75fcc
nesarikartejas10 <nesarikartejas10@gmail.com>
2026-02-10 13:46:29 +0530

    add user router

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,10 @@
 import express from "express";
+import userRouter from "./routes/user.routes.js";
 import globalErrorHandler from "./middlewares/globalErrorHandler.js";
 
 const app = express();
-
-app.get("/", (req, res) => {
-  res.send("<h1>Welcome into express server</h1>");
-});
+app.use(express.json());
+app.use("/api/v1/auth", userRouter);
 
 //global error middleware
 app.use(globalErrorHandler);
---
+++ backend/src/controllers/user.controller.js
@@ -0,0 +1,15 @@
+import asyncHandler from "../middlewares/asyncHandler.js";
+
+const registerUser = asyncHandler(async (req, res, next) => {
+  const { name, email, password } = req.body;
+
+  res
+    .status(201)
+    .json({
+      user: { name, email },
+      success: true,
+      massage: "User registered successfully",
+    });
+});
+
+export { registerUser };
